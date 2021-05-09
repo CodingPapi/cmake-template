c7c6b15e5d0ce4992eb7dc65d1bb91571102be18
Farmer Li <iamjx@qq.com>
2021-05-10 05:23:08 +0800

    add: Add the complete CMakeLists.txt template

--- src/c/main.c
+++ src/c/main.c
@@ -3,8 +3,17 @@
 
 #include <stdio.h>
 
+#include "math/add.h"
+#include "math/minus.h"
+
 int main(void) {
   printf("Hello CMake!\n");
+
+  int res = add_int(10, 24);
+  printf("10 + 24 = %d\n", res);
+
+  res = minus_int(40, 96);
+  printf("40 - 96 = %d\n", res);
 
   return 0;
 }
